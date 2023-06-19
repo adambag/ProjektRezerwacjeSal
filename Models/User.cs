1599f21faf7e422212dd31c1cf4c02b2cb10735d
Adam Bagiński <abaginski2001@gmail.com>
2023-06-19 19:49:47 +0200

    Dodaj pliki projektów.

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjektRezerwacjeSal.Models
+{
+    public class User
+    {
+        public string Role { get; set; } //User lub Admin
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
